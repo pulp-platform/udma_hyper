c90aebceb8f0182bb6512ef4b6be55d56b106dbe
Georg Rutishauser <georgr@iis.ee.ethz.ch>
2021-04-15 19:56:35 +0200

    replace `define with localparam to avoid collision

--- udma-hyperbus/src/udma_hyper_reg_if_common.sv
+++ udma-hyperbus/src/udma_hyper_reg_if_common.sv
@@ -12,17 +12,6 @@
 //// Hayate Okuhara <hayate.okuhara@unibo.it>
 
 
-// Configuration register for Hyper bus CHANNEl
-`define REG_PAGE_BOUND          5'b00000 //BASEADDR+0x00 set the page boundary.
-`define REG_T_LATENCY_ACCESS    5'b00001 //BASEADDR+0x04 set t_latency_access
-`define REG_EN_LATENCY_ADD      5'b00010 //BASEADDR+0x08 set en_latency_additional
-`define REG_T_CS_MAX            5'b00011 //BASEADDR+0x0C set t_cs_max
-`define REG_T_RW_RECOVERY       5'b00100 //BASEADDR+0x10 set t_read_write_recovery
-`define REG_T_RWDS_DELAY_LINE   5'b00101 //BASEADDR+0x14 set t_rwds_delay_line
-`define REG_T_VARI_LATENCY      5'b00110 //BASEADDR+0x18 set t_variable_latency_check
-`define N_HYPER_DEVICE          5'b00111 //BASEADDR+0x1C set the number of connected devices
-`define MEM_SEL                 5'b01000 //BASEADDR+0x20 set Memory select: HyperRAM, Hyperflash, or PSRAM 00:Hyper RAM, 01: Hyper Flash, 10:PSRAM
-`define TRANS_ID_ALLOC          5'b01001 //BASEADDR+0x30 set 2D transfer stride
 
 module udma_hyper_reg_if_common #(
                           parameter L2_AWIDTH_NOAL = 12,
@@ -52,6 +41,19 @@ module udma_hyper_reg_if_common #(
 
                              input logic [NB_CH-1:0]            busy_vec_i
                              );
+
+  // Configuration register for Hyper bus CHANNEl
+   localparam logic[4:0] REG_PAGE_BOUND         = 5'b00000; //BASEADDR+0x00 set the page boundary.
+   localparam logic[4:0] REG_T_LATENCY_ACCESS   = 5'b00001; //BASEADDR+0x04 set t_latency_access
+   localparam logic[4:0] REG_EN_LATENCY_ADD     = 5'b00010; //BASEADDR+0x08 set en_latency_additional
+   localparam logic[4:0] REG_T_CS_MAX           = 5'b00011; //BASEADDR+0x0C set t_cs_max
+   localparam logic[4:0] REG_T_RW_RECOVERY      = 5'b00100; //BASEADDR+0x10 set t_read_write_recovery
+   localparam logic[4:0] REG_T_RWDS_DELAY_LINE  = 5'b00101; //BASEADDR+0x14 set t_rwds_delay_line
+   localparam logic[4:0] REG_T_VARI_LATENCY     = 5'b00110; //BASEADDR+0x18 set t_variable_latency_check
+   localparam logic[4:0] N_HYPER_DEVICE         = 5'b00111; //BASEADDR+0x1C set the number of connected devices
+   localparam logic[4:0] MEM_SEL                = 5'b01000; //BASEADDR+0x20 set Memory select: HyperRAM, Hyperflash, or PSRAM 00:Hyper RAM, 01: Hyper Flash, 10:PSRAM
+   localparam logic[4:0] TRANS_ID_ALLOC         = 5'b01001; //BASEADDR+0x30 set 2D transfer stride
+
 
 
    logic [31:0]                                                r_t_latency_access;
@@ -105,39 +107,39 @@ module udma_hyper_reg_if_common #(
              if (cfg_valid_i & ~cfg_reg_rwn_i)
                begin
                   case (s_wr_addr)
-                    `REG_PAGE_BOUND:
+                    REG_PAGE_BOUND:
                       begin
                          r_page_bound <= cfg_data_i[2:0];
                       end
-                    `REG_T_LATENCY_ACCESS:
+                    REG_T_LATENCY_ACCESS:
                       begin
                          r_t_latency_access <= cfg_data_i[4:0];
                       end
-                    `REG_EN_LATENCY_ADD:
+                    REG_EN_LATENCY_ADD:
                       begin
                          r_en_latency_additional <= cfg_data_i[0];
                       end
-                    `REG_T_CS_MAX:
+                    REG_T_CS_MAX:
                       begin
                          r_t_cs_max  <= cfg_data_i;
                       end
-                    `REG_T_RW_RECOVERY:
+                    REG_T_RW_RECOVERY:
                       begin
                          r_t_read_write_recovery <= cfg_data_i;
                       end
-                    `REG_T_RWDS_DELAY_LINE:
+                    REG_T_RWDS_DELAY_LINE:
                       begin
                          r_t_rwds_delay_line <= cfg_data_i[DELAY_BIT_WIDTH-1:0];
                       end
-                    `REG_T_VARI_LATENCY:
+                    REG_T_VARI_LATENCY:
                       begin
                          r_t_variable_latency_check <= cfg_data_i[3:0];
                       end
-                    `N_HYPER_DEVICE:
+                    N_HYPER_DEVICE:
                       begin
                          r_n_hyperdevice <= cfg_data_i[2:0];
                       end
-                    `MEM_SEL:
+                    MEM_SEL:
                       begin
                          r_mem_sel <= cfg_data_i[1:0];
                       end
@@ -150,27 +152,27 @@ module udma_hyper_reg_if_common #(
      begin
         cfg_data_o = 32'h0;
         case (s_rd_addr)
-          `REG_PAGE_BOUND:
+          REG_PAGE_BOUND:
             cfg_data_o = {29'b0, r_page_bound};
-          `REG_T_LATENCY_ACCESS:
+          REG_T_LATENCY_ACCESS:
             cfg_data_o = {28'b0, cfg_t_latency_access_o};
-          `REG_EN_LATENCY_ADD:
+          REG_EN_LATENCY_ADD:
             cfg_data_o = {31'b0, cfg_en_latency_additional_o};
-          `REG_T_CS_MAX:
+          REG_T_CS_MAX:
             cfg_data_o = cfg_t_cs_max_o;
-          `REG_T_RW_RECOVERY:
+          REG_T_RW_RECOVERY:
             cfg_data_o = cfg_t_read_write_recovery_o;
-          `REG_T_RWDS_DELAY_LINE:
+          REG_T_RWDS_DELAY_LINE:
             cfg_data_o = r_t_rwds_delay_line;
-          `REG_T_VARI_LATENCY:
+          REG_T_VARI_LATENCY:
             cfg_data_o = {29'b0, cfg_t_variable_latency_check_o};
-          `N_HYPER_DEVICE:
+          N_HYPER_DEVICE:
             cfg_data_o = {29'h0, cfg_n_hyperdevice_o};
-          /*`PSRAM_INST:
+          /*PSRAM_INST:
             cfg_data_o = {16'h0, cfg_psram_inst_o}; */
-          `MEM_SEL:
+          MEM_SEL:
             cfg_data_o = {{30{1'b0}}, cfg_mem_sel_o};
-          `TRANS_ID_ALLOC:
+          TRANS_ID_ALLOC:
             cfg_data_o = {{(32-$clog2(NB_CH)){1'b0}},alloc_id};
           default:
             cfg_data_o = 'h0;
